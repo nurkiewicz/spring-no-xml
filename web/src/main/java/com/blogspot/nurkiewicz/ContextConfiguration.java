ac2d5a09a247366e89daa27c3f6230f27df8043c
Tomasz Nurkiewicz <nurkiewicz@gmail.com>
2011-01-09 18:50:11 +0100

    Moving JMS connection and sending to @Configuration class

--- web/src/main/java/com/blogspot/nurkiewicz/ContextConfiguration.java
+++ web/src/main/java/com/blogspot/nurkiewicz/ContextConfiguration.java
@@ -1,13 +1,20 @@
 package com.blogspot.nurkiewicz;
 
+import javax.jms.ConnectionFactory;
+import javax.jms.Queue;
 import javax.sql.DataSource;
 
+import org.apache.activemq.command.ActiveMQQueue;
+import org.apache.activemq.pool.PooledConnectionFactory;
+import org.apache.activemq.spring.ActiveMQConnectionFactory;
 import org.apache.commons.dbcp.BasicDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcOperations;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jms.core.JmsOperations;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.transaction.PlatformTransactionManager;
 
 /**
@@ -34,6 +41,25 @@ public class ContextConfiguration {
 	@Bean
 	public PlatformTransactionManager transactionManager() {
 		return new DataSourceTransactionManager(dataSource());
+	}
+
+	@Bean
+	public ConnectionFactory jmsConnectionFactory() {
+		final ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory();
+		factory.setBrokerURL("tcp://localhost:61616");
+		return new PooledConnectionFactory(factory);
+	}
+
+	@Bean
+	public Queue requestsQueue() {
+		return new ActiveMQQueue("requests");
+	}
+
+	@Bean
+	public JmsOperations jmsOperations() {
+		final JmsTemplate jmsTemplate = new JmsTemplate(jmsConnectionFactory());
+		jmsTemplate.setDefaultDestination(requestsQueue());
+		return jmsTemplate;
 	}
 
 }
